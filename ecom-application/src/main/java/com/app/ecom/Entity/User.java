32d1091be6d0f473c7239b0187166018ce89ab96
vkfullstack <vkhetre69@gmail.com>
2026-07-22 21:53:43 +0530

    refactor(entity): update User entity with address and role support

--- ecom-application/src/main/java/com/app/ecom/Entity/User.java
+++ ecom-application/src/main/java/com/app/ecom/Entity/User.java
@@ -1,10 +1,33 @@
 package com.app.ecom.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "users_table")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
+    private  String email;
+    private  String phone;
+    private  UserRole role=UserRole.CUSTOMER;
+    @OneToOne(cascade = CascadeType.ALL,orphanRemoval = true)
+    @JoinColumn(name = "address_id" ,referencedColumnName = "id")
+    private Adress adress;
+    @CreationTimestamp
+    private LocalDateTime  createdAt;
+    @UpdateTimestamp
+    private  LocalDateTime  updatedAt;
 }
